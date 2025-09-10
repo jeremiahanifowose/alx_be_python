ffe2f7babd2f57242c9eca86f503d837bed2293f
jeremiah <jeremiahanifowose026@gmail.com>
2025-09-10 18:01:30 +0100

    ainjay

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,12 @@
+principal = 1000  # Principal amount
+rate_of_interest = 5  # Annual interest rate (in %)
+time = 3  # Time (in years)
+
+# Simple Interest Calculation
+simple_interest = (principal * rate_of_interest * time) / 100
+
+# Output
+print(f"Principal: {principal}")
+print(f"Rate of Interest: {rate_of_interest}%")
+print(f"Time: {time} years")
+print(f"Simple Interest: {simple_interest}")
